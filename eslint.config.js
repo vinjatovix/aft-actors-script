db103e523295c00b9cda60196b09b39b826a4d0f
vinjadevix <vinjatovix@gmail.com>
2025-03-28 18:01:24 +0100

    [GH-3] Refactor .gitignore and package.json, and update SonarQube configuration

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import reactx from "eslint-plugin-react-x";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "coverage"] },
   {
     extends: [
       js.configs.recommended,
