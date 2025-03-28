e444a6fd4db0cba53c3b503c27d8b0597ed23f56
vinjadevix <vinjatovix@gmail.com>
2025-03-28 15:20:34 +0100

    [GH-3] Add Loader component with CSS and test file

---
+++ src/actorsScript/components/Loader.tsx
@@ -0,0 +1,11 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader" data-testid="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
