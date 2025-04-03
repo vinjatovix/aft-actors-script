3b3452a7fab17906a3bc4066e1df80f3c1c8115d
vinjatovix <vinjatovix@gmail.com>
2025-04-03 00:38:38 +0000

    [GH-18] feat: add userId filter to getAllCharacterBuildings thunk

--- src/redux/thunks/characterBuildingThunks.ts
+++ src/redux/thunks/characterBuildingThunks.ts
@@ -25,6 +25,7 @@ export const getAllCharacterBuildings = createAsyncThunk(
     try {
       const state = getState() as { auth: AuthState };
       const token = state.auth.token;
+      const userId = state.auth.user.id;
 
       if (!token) {
         throw new Error("No hay token disponible");
@@ -35,6 +36,7 @@ export const getAllCharacterBuildings = createAsyncThunk(
           "character.book.author,scene.characters,actor,relationshipCircumstances.character",
         fields:
           "startingPoint,previousCircumstances,sceneCircumstances,center,relationshipCircumstances.circumstance,relationshipCircumstances.character.name,scene.description,scene.characters,actor.username,actionUnits,center,character.name,character.book.title,character.book.author.name",
+        filter: `actor:${userId}`,
       }).toString();
       const url = `${API_MAP.characterBuildings.getAll.url}?${queryParams}`;
       const data = await handleFetch<CharacterBuilding[]>(url, {
