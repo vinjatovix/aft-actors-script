c2418630699a8f173a6b9d507736d0d16ffb265d
vinjatovix <vinjatovix@gmail.com>
2025-04-20 04:18:30 +0000

    [GH-30] test(Scenes): wrap render call in act for proper async handling

--- tests/actorsScript/pages/Scenes.test.tsx
+++ tests/actorsScript/pages/Scenes.test.tsx
@@ -1,16 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Scenes } from "../../../src/actorsScript/pages/Scenes";
 import { Provider } from "react-redux";
 import { store } from "../../../src/redux/store";
 
 describe("Scenes Page", () => {
-  it("renders the Scenes page with a title and author names", () => {
-    render(
-      <Provider store={store}>
-        render(<Scenes />);
-      </Provider>
-    )
+  it("renders the Scenes page with a title and author names", async () => {
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Scenes />
+        </Provider>
+      );
+    });
 
     expect(screen.getByRole("heading", { level: 5 })).toHaveTextContent(
       "Esceas"
