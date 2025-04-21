778be8db49a7a18914bf614b2fc815a5d5ab017a
vinjadevix <vinjatovix@gmail.com>
2025-04-22 01:44:54 +0200

    [GH-6] refactor(tests): update Books and Characters tests to use async act for rendering

--- tests/actorsScript/pages/Books.test.tsx
+++ tests/actorsScript/pages/Books.test.tsx
@@ -1,18 +1,21 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { Books } from "../../../src/actorsScript/pages/Books";
-import { Provider } from "react-redux";
-import { store } from "../../../src/redux/store";
+import { act, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Books } from '../../../src/actorsScript/pages/Books';
+import { Provider } from 'react-redux';
+import { store } from '../../../src/redux/store';
 
-describe("Books Page", () => {
-  it("renders the Books page with a title", () => {
-    render(
-      <Provider store={store}>
-        <Books />
-      </Provider>
-    );
-    expect(screen.getByRole("heading", { level: 5 })).toHaveTextContent(
-      "Obras"
+describe('Books Page', () => {
+  it('renders the Books page with a title', async () => {
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Books />
+        </Provider>
+      );
+    });
+
+    expect(screen.getByRole('heading', { level: 5 })).toHaveTextContent(
+      'Obras'
     );
   });
 });
--- tests/actorsScript/pages/Characters.test.tsx
+++ tests/actorsScript/pages/Characters.test.tsx
@@ -1,19 +1,23 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { Characters } from "../../../src/actorsScript/pages/Characters";
-import { store } from "../../../src/redux/store";
-import { Provider } from "react-redux";
+import { act, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Characters } from '../../../src/actorsScript/pages/Characters';
+import { store } from '../../../src/redux/store';
+import { Provider } from 'react-redux';
 
-describe("Characters Page", () => {
-  it("renders the Characters page with a title and author names", () => {
-    render(
-      <Provider store={store}>
-        render(<Characters />);
-      </Provider>
-    )
+describe('Characters Page', () => {
+  it('renders the Characters page with a title and author names', async () => {
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          render(
+          <Characters />
+          );
+        </Provider>
+      );
+    });
 
-    expect(screen.getByRole("heading", { level: 5 })).toHaveTextContent(
-      "Persoaxes"
+    expect(screen.getByRole('heading', { level: 5 })).toHaveTextContent(
+      'Persoaxes'
     );
   });
 });
