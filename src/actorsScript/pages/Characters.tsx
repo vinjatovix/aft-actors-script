c14350d8f3163607367c7a72b5beb807b8ab3244
vinjatovix <vinjatovix@gmail.com>
2025-04-27 18:08:40 +0000

    [GH-33] feat(characters): enhance Characters page with book-based character fetching and navigation button

--- src/actorsScript/pages/Characters.tsx
+++ src/actorsScript/pages/Characters.tsx
@@ -1,17 +1,27 @@
 import { useEffect, useMemo } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../redux/types';
-import { getAllCharacters } from '../../redux/thunks/characterThunks';
+import {
+  getAllCharacters,
+  getCharactersByBookId
+} from '../../redux/thunks/characterThunks';
 import { Character } from '../../redux/interfaces/characterInterfaces';
 import { CharacterCard } from '../components/characters/CharacterCard';
 import Groups3OutlinedIcon from '@mui/icons-material/Groups3Outlined';
 import { PageHeader } from '../components/PageHeader';
 import { useTranslation } from 'react-i18next';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 export const Characters = () => {
   const { t } = useTranslation('common');
 
   const dispatch = useDispatch<AppDispatch>();
+
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const bookId = params.get('book');
+  const navigate = useNavigate();
 
   const { characters, loading, error } = useSelector(
     (state: RootState) => state.character,
@@ -19,8 +29,12 @@ export const Characters = () => {
   );
 
   useEffect(() => {
-    dispatch(getAllCharacters());
-  }, [dispatch]);
+    if (bookId) {
+      dispatch(getCharactersByBookId(bookId));
+    } else {
+      dispatch(getAllCharacters());
+    }
+  }, [dispatch, bookId]);
 
   const characterCards = useMemo(
     () =>
@@ -43,6 +57,20 @@ export const Characters = () => {
       {error && <p>Error: {error}</p>}
 
       {!loading && !characters.length && <p>{t('noAvailable')}</p>}
+
+      {/* {bookId && (
+        <Button
+          variant="outlined"
+          color="info"
+          onClick={() => navigate('/characters')}
+          className="reset-filter-button"
+          size="small"
+          sx={{ ml: '1rem' }}
+        >
+          {t('seeAll')}
+        </Button>
+      )} */}
+
       {!loading && !!characters.length && (
         <div className="card-container">{characterCards}</div>
       )}
--- tests/actorsScript/pages/Characters.test.tsx
+++ tests/actorsScript/pages/Characters.test.tsx
@@ -1,22 +1,15 @@
-import { act, render, screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Characters } from '../../../src/actorsScript/pages/Characters';
-import { store } from '../../../src/redux/store';
-import { Provider } from 'react-redux';
 import i18n from '../../../src/i18n';
+import { renderWithProviders } from '../../test-utils/renderWithProviders';
 
 const t = (key: string, ns: string = 'common') => i18n.t(key, { ns });
 
 describe('Characters Page', () => {
   it('renders the Characters page with a title and author names', async () => {
     await act(async () => {
-      render(
-        <Provider store={store}>
-          render(
-          <Characters />
-          );
-        </Provider>
-      );
+      renderWithProviders({ ui: <Characters /> });
     });
 
     expect(screen.getByRole('heading', { level: 5 })).toHaveTextContent(
