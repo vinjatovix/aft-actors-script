188edcad3bc67d2d3caf638a69f7a7e09bb86602
vinjatovix <vinjatovix@gmail.com>
2025-04-27 18:07:15 +0000

    [GH-33] feat(authors): implement search functionality and loading/error states in Authors page

--- tests/actorsScript/pages/Authors.test.tsx
+++ tests/actorsScript/pages/Authors.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Authors } from '../../../src/actorsScript/pages/Authors';
 import i18n from '../../../src/i18n';
@@ -7,8 +7,10 @@ import { renderWithProviders } from '../../test-utils/renderWithProviders';
 const t = (key: string, ns: string = 'common') => i18n.t(key, { ns });
 
 describe('Authors Page', () => {
-  it('renders the Authors page with a title and author names', () => {
-    renderWithProviders({ ui: <Authors /> });
+  it('renders the Authors page with a title and author names', async () => {
+    await act(async () => {
+      renderWithProviders({ ui: <Authors /> });
+    })
 
     expect(screen.getByRole('heading', { level: 5 })).toHaveTextContent(
       t('authors')
